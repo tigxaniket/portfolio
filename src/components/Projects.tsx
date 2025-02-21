110dedcd60230678d62993663110b0bd553d57b7
tigxaniket <tigxaniket@gmail.com>
2025-02-21 07:35:22 +0530

    src/components/Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -26,7 +26,7 @@ const Projects = () => {
             </h2>
           </div>
 
-          <div className="grid lg:grid-cols-4 gap-8">
+          <div className="grid lg:grid-cols-4 gap-4">
             <div className="lg:col-span-3">
               <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {projects.map((project, index) => (
