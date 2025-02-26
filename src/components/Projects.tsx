cbf8d0f40389a838f9cadf4c9a8ea1ae95b3c41e
tigxaniket <tigxaniket@gmail.com>
2025-02-26 07:00:02 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -14,8 +14,8 @@ const Projects = () => {
        title: "Portfolio Website",
       description: "A full-featured online shopping platform built with React and Node.js",
 @@ -14,12 +21,6 @@ const Projects = () => {
-      description: "Enhanced user experience,Increased mobile traffic by 20%",
-      tags: ["Next.js", "Tailwind"],
+      description: "Enhanced user experience,Increased mobile traffic by 20%,",
+      tags: ["Typescript", "Tailwind"],
       link: "#https://tigxaniket.netlify.app"
     },
   ];
